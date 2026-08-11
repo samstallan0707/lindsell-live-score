dc50179390d7155e0dbc997a4fbbaad47ba6ce91
samstallan0707 <samstallan0707@gmail.com>
2026-08-11 14:31:04 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.LINDSELL_ONLINE_CONFIG = {
+  supabaseUrl: "https://woycxdbgctgkveclnxrf.supabase.co",
+  publishableKey: "sb_publishable_SwhK7bYiPlIMI_yHErpdBA_WMZOSlWO",
+  rowId: 1,
+  staleAfterHours: 4,
+  pollSeconds: 3,
+  playCricketUrl: "https://lindsell.play-cricket.com/"
+};
